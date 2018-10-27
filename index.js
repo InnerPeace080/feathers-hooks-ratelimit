011dab05389688f2d8ce7ff209c58b81bcfd91b6
tranphuong.080 <tranphuong.080@gmail.com>
2018-10-27 10:58:31 +0700

    support 2 IdKey

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const FastRateLimit = require('fast-ratelimit').FastRateLimit;
 const errors = require('@feathersjs/errors');
 
 module.exports = function(options = {}) {
-  const { threshold, ttl, namespace, userIdKey, errorMessage, errorData } = options;
+  const { threshold, ttl, namespace, userIdKey, userIdKey2, errorMessage, errorData } = options;
   const messageLimiter = new FastRateLimit({ threshold, ttl });
 
   return async context => {
@@ -10,9 +10,11 @@ module.exports = function(options = {}) {
 
     if (namespace) {
       _namespace = namespace;
-    } else {
+    } else if(context.params && context.params.user) {
       const user = context.params.user;
-      _namespace = user ? user[userIdKey] : 'default';
+      _namespace = user[userIdKey] || user[userIdKey2] || 'default';
+    } else{
+      _namespace = 'default';
     }
 
     try {
